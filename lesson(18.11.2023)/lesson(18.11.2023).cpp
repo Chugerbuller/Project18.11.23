0f38ccfc6cc24383cb9ef2572526f8536324f79c
Andrey Chugainov <sprunk223@gmail.com>
2023-11-19 16:36:00 +0500

    count

--- lesson(18.11.2023)/lesson(18.11.2023).cpp
+++ lesson(18.11.2023)/lesson(18.11.2023).cpp
@@ -3,13 +3,13 @@
 int main()
 {
 	const char delimiter = '_';
-	string input_path = "D:\\code\\lessons\\lesson(18.11.2023)\\lesson(18.11.2023)\\variables.txt";
-	string output_path = "D:\\code\\lessons\\lesson(18.11.2023)\\lesson(18.11.2023)\\output.txt";
-	string incorrect_delimiters = "*/- ;";
-	vector<string> lines = GetLines(input_path);
+	string input_path = "D:\\code\\lessons\\lesson18.11.2023\\lesson(18.11.2023)\\variables.txt";
+	string output_path = "D:\\code\\lessons\\lesson18.11.2023\\lesson(18.11.2023)\\output.txt";
+	string incorrect_delimiters = "*/- ;&?.,|";
+	vector<string> lines = GetLines(input_path, &total_lines);
 	
-	for (size_t i = 0; i < lines.size(); i++)
-	{
+	for (size_t i = 0; i < lines.size(); i++){
+		string temp = lines[i];
 		CorrectLines(&lines[i], delimiter, incorrect_delimiters);
 	}
 
